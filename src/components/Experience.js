bcd000e1908e011a4989bab4abc2af72bc867e46
Rafael Herrera <santoElias65537@gmail.com>
2021-12-05 00:20:56 -0700

    ADDED layout='1-column-left' to experience component, hopefully this makes localhost and deployed look the same

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,27 +11,11 @@ class Experience extends Component {
     if (this.props.resumeExperience && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.experience;
       var work = this.props.resumeExperience.map(function (work, i) {
-        // const technologies = work.technologies;
-        // const mainTechnologies = work.mainTech;
-
-        // var mainTech = mainTechnologies.map((technology, i) => {
-        //   return (
-        //     <Badge pill className="main-badge mr-2 mb-2" key={i}>
-        //       {technology}
-        //     </Badge>
-        //   );
-        // });
-        // var tech = technologies.map((technology, i) => {
-        //   return (
-        //     <Badge pill className="experience-badge mr-2 mb-2" key={i}>
-        //       {technology}
-        //     </Badge>
-        //   );
-        // });
+       
         return (
-          <VerticalTimelineElement
+          <VerticalTimelineElement            
             className="vertical-timeline-element--work"
-            date={work.years}        
+            date={work.years}                   
             iconStyle={{
               background: "#AE944F" ,
               color: "#fff",
@@ -39,10 +23,9 @@ class Experience extends Component {
             }}
             icon={<i className="fas fa-angle-right experience-icon"></i>}
             key={i}
+            
           >
-            {/* <div style={{ textAlign: "left", marginBottom: "4px" }}>
-              {mainTech}
-            </div> */}
+           
 
             <h3
               className="vertical-timeline-element-title"
@@ -51,7 +34,6 @@ class Experience extends Component {
               {work.title}
             </h3>
            
-            {/* <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div> */}
           </VerticalTimelineElement>
         );
       });
@@ -69,7 +51,7 @@ class Experience extends Component {
           </div>
         </div>
         <div className="col-md-8 mx-auto">
-          <VerticalTimeline>
+          <VerticalTimeline  layout='1-column-left'>
             {work}            
           </VerticalTimeline>
         </div>
